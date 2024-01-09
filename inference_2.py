3e62492449491584dbe3587b37cd0f43f2ac7873
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 19:30:31 +0200

    seccond inference

---
+++ inference_2.py
@@ -0,0 +1,11 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+model_id = "saved_models/try_1"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
+
+prompt = "A photo of sks table tent in a restaurant"
+image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+
+from time import time
+image.save(f"tent-restaurant-{int(time())}.png")
