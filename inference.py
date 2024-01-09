9c9ca2d2add5b56bf8e68780acdb7630727236a5
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 21:24:40 +0200

    prompt

--- inference.py
+++ inference.py
@@ -8,10 +8,10 @@ prompt = "A photo of sks table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
-image.save(f"tent-restaurant-1.png")
+image.save(f"tent-car-1.png")
 
 prompt = "A photo of a table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
-image.save(f"prior-tent-restaurant-1.png")
+image.save(f"prior-tent-car-1.png")
