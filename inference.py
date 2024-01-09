904e1c2e1a9303d57ec3bd3b6fc9d6c316c1df14
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 21:35:47 +0200

    r

--- inference.py
+++ inference.py
@@ -1,20 +1,17 @@
-from diffusers import StableDiffusionPipeline, UNet2DConditionModel, DiffusionPipeline
+from diffusers import StableDiffusionPipeline, UNet2DConditionModel
 import torch
 
-
-unet = UNet2DConditionModel.from_pretrained("/kaggle/working/saved_models/try_1/checkpoint-500", subfolder="unet", use_safetensors=True)
-
 model_id = "saved_models/try_1"
-pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, unet=unet).to("cuda")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
 prompt = "A photo of sks table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
-image.save(f"500-tent-car-1.png")
+image.save(f"tent-car-1.png")
 
 prompt = "A photo of a table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
-image.save(f"500-prior-tent-car-1.png")
+image.save(f"prior-tent-car-1.png")
