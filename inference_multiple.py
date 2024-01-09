cb1aa098d43cf66307026bfa8bf1c8917bdbdd8c
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 23:27:30 +0200

    upd

--- inference_multiple.py
+++ inference_multiple.py
@@ -5,9 +5,10 @@ model_id = "saved_models/try_2"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
 for (a, b) in [
-    ("on a car", "car"),
-    ("on a cat", "cat"),
-    ("as a human hat", "hat"),
+    # ("on a car", "car"),
+    # ("on a cat", "cat"),
+    # ("as a human hat", "hat"),
+    ("that is red", "red"),
 ]:
     prompt = f"A photo of sks table tent {a}"
     image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
