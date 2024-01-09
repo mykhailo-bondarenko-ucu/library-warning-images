d0e979089b248a08fd101196a5df61f4fa904e24
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 21:24:04 +0200

    prompt

--- inference.py
+++ inference.py
@@ -4,13 +4,13 @@ import torch
 model_id = "saved_models/try_1"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-prompt = "A photo of sks table tent in a restaurant"
+prompt = "A photo of sks table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
 image.save(f"tent-restaurant-1.png")
 
-prompt = "A photo of a table tent in a restaurant"
+prompt = "A photo of a table tent on a car"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
 # from time import time
