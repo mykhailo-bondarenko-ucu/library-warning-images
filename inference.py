010c8b64aecd1ff161f4e5e2171519512b287561
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 18:41:14 +0200

    add inference code

---
+++ inference.py
@@ -0,0 +1,19 @@
+from accelerate import Accelerator
+from diffusers import DiffusionPipeline
+
+model_id = "CompVis/stable-diffusion-v1-4"
+pipeline = DiffusionPipeline.from_pretrained(model_id)
+
+accelerator = Accelerator()
+
+unet, text_encoder = accelerator.prepare(pipeline.unet, pipeline.text_encoder)
+
+accelerator.load_state("saved_models/try_1")
+
+pipeline = DiffusionPipeline.from_pretrained(
+    model_id,
+    unet=accelerator.unwrap_model(unet),
+    text_encoder=accelerator.unwrap_model(text_encoder),
+)
+
+pipeline.save_pretrained("dreambooth-pipeline")
