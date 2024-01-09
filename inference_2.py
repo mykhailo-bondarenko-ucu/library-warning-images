28c50d2f86b8bbcf50a2be25ff17b18b8bd02a13
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 19:31:43 +0200

    change naming

--- inference_2.py
+++ inference_2.py
@@ -7,5 +7,5 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float
 prompt = "A photo of sks table tent in a restaurant"
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
-from time import time
-image.save(f"tent-restaurant-{int(time())}.png")
+# from time import time
+image.save(f"tent-restaurant-2.png")
