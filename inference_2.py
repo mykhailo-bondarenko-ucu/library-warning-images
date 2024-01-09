b4ebd083bb3ae6362014e21f3055cc53b5aa48c6
michael-2956 <work.michael.2956@gmail.com>
2024-01-09 21:41:01 +0200

    improve second inference

--- inference_2.py
+++ inference_2.py
@@ -4,14 +4,18 @@ import torch
 model_id = "saved_models/try_2"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-prompt = "A photo of sks table tent in a restaurant"
-image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+for (a, b) in [
+    ("in a restaurant", "restaurant"),
+    ("on a car", "car"),
+]:
+    prompt = f"A photo of sks table tent {a}"
+    image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
-# from time import time
-image.save(f"tent-restaurant-2.png")
+    # from time import time
+    image.save(f"tent-{b}-2.png")
 
-prompt = "A photo of a table tent in a restaurant"
-image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
+    prompt = f"A photo of a table tent {a}"
+    image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
 
-# from time import time
-image.save(f"prior-tent-restaurant-2.png")
+    # from time import time
+    image.save(f"prior-tent-{b}-2.png")
